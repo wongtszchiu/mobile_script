bd6eb88b1d7aaa776c17cf2109306eecc97dc562
Chiu <34164153+wongtszchiu@users.noreply.github.com>
2024-02-15 16:08:31 +0000

    add hkt

---
+++ hkt.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        hkt (git)
+// @namespace   hkt
+// @match       https://queue.hkticketing.com/hotshow.html
+// @icon        https://d2jp7hosdnx5my.cloudfront.net/d/images/logo.png
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 2023/11/28 下午2:05:24
+// @updateURL   https://github.com/wongtszchiu/mobile_script/raw/main/hkt.user.js
+// ==/UserScript==
+
+setTimeout(()=>
+    location.replace('https://m-entry-hotshow.hkticketing.com/')
+,3500);
