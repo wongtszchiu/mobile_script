bfd6c8da8efb1105066113764b5dfe7feb420dd8
Chiu <34164153+wongtszchiu@users.noreply.github.com>
2024-02-27 23:27:40 +0800

    Update queue.user.js

--- queue.user.js
+++ queue.user.js
@@ -4,7 +4,7 @@
 // @match       https://msg.cityline.com/*
 // @icon        https://www.cityline.com/assets/img/citylinelogo.png
 // @grant       none
-// @version     1.2
+// @version     1.3
 // @author      -
 // @description 2023/11/28 下午2:05:24
 // @updateURL   https://github.com/wongtszchiu/mobile_script/raw/main/queue.user.js
