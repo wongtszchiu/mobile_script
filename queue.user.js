e4ed1461be7a74e7fa221bf2db6bb957630a4968
Chiu <34164153+wongtszchiu@users.noreply.github.com>
2024-03-06 13:11:23 +0800

    Update queue.user.js

--- queue.user.js
+++ queue.user.js
@@ -4,15 +4,19 @@
 // @match       https://msg.cityline.com/*
 // @icon        https://www.cityline.com/assets/img/citylinelogo.png
 // @grant       none
-// @version     1.5
+// @version     1.6
 // @author      -
 // @description 2023/11/28 下午2:05:24
 // @updateURL   https://github.com/wongtszchiu/mobile_script/raw/main/queue.user.js
 // ==/UserScript==
 
 let rootInterval = null;
-rootInterval = setInterval( ()=>{
+rootInterval = setInterval(()=>{
 
-    document.querySelector("#btn-retry-en-1").click();
-
-  },6000);
+    if(!document.querySelector("#btn-retry-en-1").disabled){
+        setTimeout(()=>
+            document.querySelector("#btn-retry-en-1").click()
+        ,3000);
+    }
+    
+  },1000);
